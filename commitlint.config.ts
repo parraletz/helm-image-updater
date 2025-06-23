5ae5e0ad088ac9f0707316b64bf776b3a3b0523e
Alex Parra <parraletz@gmail.com>
2025-06-23 14:49:25 -0600

    chore: Enhance commitlint configuration with type enumeration rules
    - Added 'type-enum' rule to enforce specific commit message types for better consistency.
    - Included types such as 'feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert', and 'wip'.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,6 +4,24 @@ const config: UserConfig = {
   extends: ['@commitlint/config-angular'],
   rules: {
     'subject-case': [2, 'always', 'sentence-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+      ],
+    ],
   },
 }
 
